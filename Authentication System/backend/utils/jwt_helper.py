8c54460daf05486a07e65eb3a0087a56410bc411
Aditya Marne <120710171+AdityaMarne@users.noreply.github.com>
2025-04-14 15:10:31 +0530

    Delete Authentication System directory

--- Authentication System/backend/utils/jwt_helper.py
+++
@@ -1,9 +0,0 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
-
-def generate_tokens(identity):
-    access = create_access_token(identity = str(identity))
-    refresh = create_refresh_token(identity = str(identity))
-    return {"access": access, "refresh": refresh}
-
-
-
--- Authentication System/backend/utils/token_generator.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from datetime import datetime, timedelta
-
-def generate_reset_token():
-    return str(uuid.uuid4())
-
-def token_expiry(hours=1):
-    return datetime.utcnow() + timedelta(hours=hours)
